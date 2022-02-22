fed3f8201c71f66e77882a917c59e35759e1efac
Moez <mrbouaggadmoez@gmail.com>
2022-02-22 18:44:46 +0100

    bug fix

--- src/decorators/withHTML.ts
+++ src/decorators/withHTML.ts
@@ -22,7 +22,7 @@ export const withHTML = makeDecorator({
 
       if (root) {
         let rootClone = root.cloneNode(true) as HTMLElement;
-
+        rootClone.style.width = "fit-content";
         document.body.appendChild(rootClone);
         const rootRect = rootClone.getBoundingClientRect();
         width = rootRect.width;
