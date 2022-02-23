04093d62e9e1bb3193f49a507db30562e5c2d3a9
Moez <mrbouaggadmoez@gmail.com>
2022-02-23 12:22:31 +0100

    Fix addon register issue

--- preset.js
+++ preset.js
@@ -1,1 +1,15 @@
-require("./dist/preset");
+function managerEntries(entry = []) {
+  return [...entry, require.resolve("./dist/register")];
+}
+
+function config(entry = [], { addDecorator = true }) {
+  const addonConfig = [];
+  if (addDecorator) {
+    addonConfig.push(require.resolve("./dist/addDecorators"));
+  }
+  return [...entry, ...addonConfig];
+}
+module.exports = {
+  managerEntries,
+  config,
+};
