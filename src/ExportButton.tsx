f50b631b21509f1365e701453b8a3396759783ef
Bouaggad Moez <mrbouaggadmoez@gmail.com>
2022-06-20 10:29:25 +0100

    fix: remove react-tiny-popover (#25)

--- src/ExportButton.tsx
+++ src/ExportButton.tsx
@@ -13,7 +13,6 @@ import {
   ArgTypes,
 } from "@storybook/api";
 import { Args } from "@storybook/addons";
-import { Popover } from "react-tiny-popover";
 import {
   get,
   has,
@@ -42,7 +41,6 @@ import {
   EXPORT_PROGRESS,
   EXPORT_SINGLE_STORY,
   EXPORT_ALL_STORIES,
-  IFRAME_RENDERER_CLICK,
   GET_AUTH,
   SET_AUTH,
   SAMPLE_STORYBOOK_HOST,
@@ -372,16 +370,13 @@ export const ExportButton: React.FC<SProps> = () => {
     isAuthenticated: false,
     message: "",
   });
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const argTypes = useRef({});
 
   useChannel({
     [SET_AUTH]: ({ isAuthenticated, message = "" }) => {
       setAuthState({ isAuthenticated, message });
     },
-    [IFRAME_RENDERER_CLICK]: () => {
-      setIsPopoverOpen(false);
-    },
+
     [EXPORT_START]: () => {
       setIsExporting(true);
     },
@@ -402,7 +397,6 @@ export const ExportButton: React.FC<SProps> = () => {
   // Export button click trigger (triggered only in the main thread)
   const handleExportClick = (action: string) => {
     doExport(api, state, action);
-    setIsPopoverOpen(false);
   };
 
   const handleExportError = (e: any) => {
@@ -495,109 +489,67 @@ export const ExportButton: React.FC<SProps> = () => {
   }, []);
 
   return (
-    <>
-      <div
-        style={{
-          fontFamily: "Mulish, sans-serif",
-          visibility: "hidden",
-          position: "fixed",
-          zIndex: -1,
-        }}
-      >
-        Anima
-      </div>
-      <Popover
-        isOpen={isPopoverOpen}
-        onClickOutside={() => setIsPopoverOpen(false)}
-        positions={["bottom", "left"]} // preferred positions by priority
-        content={
-          <div className="sb_popover_content">
-            <ul className="sb_popover_list">
-              <li
-                className="sb_popover_list_item"
-                onClick={() => handleExportClick(EXPORT_SINGLE_STORY)}
-              >
-                Export this component
-              </li>
-              {/* <li
-                onClick={() => handleExportClick(EXPORT_ALL_STORIES)}
-                className="sb_popover_list_item"
-              >
-                Export full library
-              </li> */}
-            </ul>
-          </div>
+    <IconButton
+      id="export-button"
+      title={
+        authState.isAuthenticated ? "Export to Anima" : "Authenticate to export"
+      }
+      onClick={() => {
+        if (isMainThread && !authState.isAuthenticated) {
+          notify(authState.message);
+          return;
         }
-      >
-        <div>
-          <IconButton
-            id="export-button"
-            title={
-              authState.isAuthenticated
-                ? "Export to Anima"
-                : "Authenticate to export"
-            }
-            onClick={() => {
-              if (isMainThread && !authState.isAuthenticated) {
-                notify(authState.message);
-                return;
-              }
-              setIsPopoverOpen(!isPopoverOpen);
-            }}
-          >
-            {isExporting ? (
-              <svg
-                width="16px"
-                height="16px"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 478 522"
-              >
-                <path
-                  className="t"
-                  d="M52.655 55h355.457a2.608 2.608 0 0 1 2.247 1.21 2.599 2.599 0 0 1 .147 2.546 398.689 398.689 0 0 1-134.045 153.408c-92.415 62.352-185.475 68.791-223.778 69.135A2.625 2.625 0 0 1 50 278.672V57.628A2.63 2.63 0 0 1 52.655 55Z"
-                  fill="#FF6250"
-                />
-                <path
-                  className="c"
-                  d="M129.375 467.75c43.835 0 79.37-35.536 79.37-79.371 0-43.834-35.535-79.369-79.37-79.369-43.835 0-79.37 35.535-79.37 79.369 0 43.835 35.535 79.371 79.37 79.371Z"
-                  fill="#FFDF90"
-                />
-                <path
-                  className="l"
-                  d="M310.854 464.542c-22.453-8.571-34.395-33.281-26.787-55.156l59.917-170.984c7.677-21.875 32.098-32.648 54.552-24.077 22.453 8.585 34.395 33.281 26.787 55.169l-59.917 170.985c-7.677 21.875-32.098 32.662-54.552 24.063Z"
-                  fill="#36F"
-                />
-              </svg>
-            ) : (
-              <svg
-                style={{
-                  ...(!authState.isAuthenticated
-                    ? { filter: "grayscale(1)" }
-                    : {}),
-                }}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 32 32"
-              >
-                <rect width="32" height="32" fill="#3B3B3B" rx="4" />
-                <path
-                  fill="#FF6250"
-                  d="M7.1287 6H24.353a.1262.1262 0 0 1 .1088.0586.1266.1266 0 0 1 .0072.1234 19.319 19.319 0 0 1-6.4955 7.4335c-4.4781 3.0214-8.9875 3.3334-10.8435 3.35a.1261.1261 0 0 1-.12-.0779.1282.1282 0 0 1-.01-.0494V6.1273A.1274.1274 0 0 1 7.1287 6Z"
-                />
-                <path
-                  fill="#FFDF90"
-                  d="M10.8461 25.9999c2.1241 0 3.846-1.7219 3.846-3.846 0-2.1242-1.7219-3.8461-3.846-3.8461C8.7219 18.3078 7 20.0297 7 22.1539c0 2.1241 1.722 3.846 3.8461 3.846Z"
-                />
-                <path
-                  fill="#36F"
-                  d="M18.708 25.7722c-1.088-.4153-1.6667-1.6127-1.298-2.6727l2.9034-8.2855c.372-1.06 1.5554-1.582 2.6434-1.1667 1.088.4161 1.6667 1.6127 1.298 2.6734l-2.9034 8.2855c-.372 1.06-1.5553 1.5827-2.6434 1.166Z"
-                />
-              </svg>
-            )}
-          </IconButton>
-        </div>
-      </Popover>
-    </>
+        handleExportClick(EXPORT_SINGLE_STORY);
+      }}
+    >
+      {isExporting ? (
+        <svg
+          width="16px"
+          height="16px"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 478 522"
+        >
+          <path
+            className="t"
+            d="M52.655 55h355.457a2.608 2.608 0 0 1 2.247 1.21 2.599 2.599 0 0 1 .147 2.546 398.689 398.689 0 0 1-134.045 153.408c-92.415 62.352-185.475 68.791-223.778 69.135A2.625 2.625 0 0 1 50 278.672V57.628A2.63 2.63 0 0 1 52.655 55Z"
+            fill="#FF6250"
+          />
+          <path
+            className="c"
+            d="M129.375 467.75c43.835 0 79.37-35.536 79.37-79.371 0-43.834-35.535-79.369-79.37-79.369-43.835 0-79.37 35.535-79.37 79.369 0 43.835 35.535 79.371 79.37 79.371Z"
+            fill="#FFDF90"
+          />
+          <path
+            className="l"
+            d="M310.854 464.542c-22.453-8.571-34.395-33.281-26.787-55.156l59.917-170.984c7.677-21.875 32.098-32.648 54.552-24.077 22.453 8.585 34.395 33.281 26.787 55.169l-59.917 170.985c-7.677 21.875-32.098 32.662-54.552 24.063Z"
+            fill="#36F"
+          />
+        </svg>
+      ) : (
+        <svg
+          style={{
+            ...(!authState.isAuthenticated ? { filter: "grayscale(1)" } : {}),
+          }}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 32 32"
+        >
+          <rect width="32" height="32" fill="#3B3B3B" rx="4" />
+          <path
+            fill="#FF6250"
+            d="M7.1287 6H24.353a.1262.1262 0 0 1 .1088.0586.1266.1266 0 0 1 .0072.1234 19.319 19.319 0 0 1-6.4955 7.4335c-4.4781 3.0214-8.9875 3.3334-10.8435 3.35a.1261.1261 0 0 1-.12-.0779.1282.1282 0 0 1-.01-.0494V6.1273A.1274.1274 0 0 1 7.1287 6Z"
+          />
+          <path
+            fill="#FFDF90"
+            d="M10.8461 25.9999c2.1241 0 3.846-1.7219 3.846-3.846 0-2.1242-1.7219-3.8461-3.846-3.8461C8.7219 18.3078 7 20.0297 7 22.1539c0 2.1241 1.722 3.846 3.8461 3.846Z"
+          />
+          <path
+            fill="#36F"
+            d="M18.708 25.7722c-1.088-.4153-1.6667-1.6127-1.298-2.6727l2.9034-8.2855c.372-1.06 1.5554-1.582 2.6434-1.1667 1.088.4161 1.6667 1.6127 1.298 2.6734l-2.9034 8.2855c-.372 1.06-1.5553 1.5827-2.6434 1.166Z"
+          />
+        </svg>
+      )}
+    </IconButton>
   );
 };
