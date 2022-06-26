47d57037e691e5077f04f43893cbc6c132d9fd0a
Amir Lellouche <amir@animaapp.com>
2022-06-26 11:31:53 +0300

    fix: fix story to contain one good sub-component

--- .storybook/stories/MUIButton.stories.jsx
+++ .storybook/stories/MUIButton.stories.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-import { AddIcCallOutlined, Delete } from '@mui/icons-material';
+import { AddIcCallOutlined, Delete as DeleteIcon } from '@mui/icons-material';
+
+// For some reason the get react component name method doesn't work for MUI Icons out of the box.
+const Delete = () => <DeleteIcon />
 
 export default {
   title: 'Example/MUI Button',
@@ -10,7 +13,7 @@ export default {
     color: { control: { type: 'select', }, options: ['primary', "secondary", "warning", "info", "success"] },
     size: { control: { type: 'select', }, options: ['small', 'medium', 'large'] },
     disabled: { control: 'boolean' },
-    startIcon: { control: { type: 'select' }, options: ['add', 'delete', 'none'], mapping: { none: null, add: <AddIcCallOutlined />, delete: <Delete /> } },
+    startIcon: { control: { type: 'select' }, options: ['addCall', 'delete', 'none'], mapping: { none: null, addCall: <AddIcCallOutlined />, delete: <Delete /> } },
   },
 };
 
